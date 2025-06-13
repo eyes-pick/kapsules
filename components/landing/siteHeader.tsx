7ed1e4cf3a789796d3dc980d8cfa165c5e87e823
lachlan C Walshe <genr8.ai@gmail.com>
2025-06-14 09:03:17 +1000

    feat: update color variables in globals.css and enhance MenubarTrigger styles

--- components/landing/siteHeader.tsx
+++ components/landing/siteHeader.tsx
@@ -5,35 +5,40 @@ export default function SiteHeader() {
     return (
         <header className="sticky top-0 z-50 w-full bg-zinc-900 backdrop-blur-md border-b border-zinc-800 shadow-md">
             <div className="max-w-6xl mx-auto flex flex-cols-[3fr] h-16 px-6">
-                {/* Center: Navigation */}
+                {/* left: Navigation */}
                 <div className="flex items-center gap-2">
                     <Menubar className="bg-transparent justify-between flex items-center gap-6 text-green-700 focus:text-green-700/60  max-w-[73px] outline-0 border-0 shadow-0">
                         <MenubarMenu>
-                            <MenubarTrigger className="bg-none text-green-700 focus:text-green-700/60 text-2xl max-w-[28px] hover:text-3xl outline-0 border-0 shadow-0">◱</MenubarTrigger>
+                            <MenubarTrigger className="bg-none text-green-700 focus:text-green-700/60 text-xl max-w-[28px] hover:text-green-100/30 hover:text-2xl outline-0 border-0 shadow-0 mr-6">◱</MenubarTrigger>
                             <MenubarContent>
-                                <MenubarItem>Home</MenubarItem>
-                                <MenubarItem>Starters</MenubarItem>
-                                <MenubarItem>Pro</MenubarItem>
-                                <MenubarItem>Discover</MenubarItem>
+                                <MenubarItem><a href="/subscribe">Pricing</a></MenubarItem>
+                                <MenubarItem><a href="/learn">Learning</a></MenubarItem>
+                                <MenubarItem><a href="/templates">Templates</a></MenubarItem>
+                                <MenubarItem><a href="/compete">Competitions</a></MenubarItem>
+                                <MenubarItem><a href="/docs">Documentation</a></MenubarItem>
                             </MenubarContent>
                         </MenubarMenu>
                     </Menubar>
                 </div>
                 {/* Left: Logo/Brand */}
                 <div className="flex items-center gap-2">
-                    <Label className="text-2xl font-bold px-3 py-2 tracking-tight text-green-300">Kapsules</Label>
+                    <Label className="text-3xl font-bold px-3 py-2 tracking-tight text-green-50"><a href="./">Kapsules</a></Label>
                 </div>
 
                 {/* Right: Account/Settings */}
-                <div className="flex items-center gap-2">
-                    <Menubar className="bg-transparent justify-between md:flex items-center gap-6 text-green-700 focus:text-green-700/60 hover:text-lg max-w-[73px] outline-0 border-0 shadow-0">
+                <div className="flex items-center">
+                    <Menubar className="bg-transparent justify-between md:flex items-center gap-8 text-green-700 focus:text-green-700/60 hover:text-green-100/30 max-w-[50px] outline-0 border-0 shadow-0">
                         <MenubarMenu>
-                            <MenubarTrigger className="text-green-700 focus:text-green-700/60 hover:text-lg max-w-[73px] outline-0 border-0 sgadow-0">Profile</MenubarTrigger>
+                            <MenubarTrigger className="text-green-500/55 focus:text-green-700/60 hover:text-green-100/30 max-w-[50px] outline-0 border-0 shadow-0"><a href="./learn">Discover</a></MenubarTrigger>
+                            <MenubarTrigger className="text-green-500/55 focus:text-green-700/60 hover:text-green-100/30 max-w-[50px] outline-0 border-0 shadow-0 mr-4"><a href="/templates">Templates</a></MenubarTrigger>
+                        </MenubarMenu>
+                        <MenubarMenu>
+                            <MenubarTrigger className="text-green-500/55 focus:text-green-700/60 hover:text-green-100/30 max-w-[50px] outline-0 border-0 shadow-0">Profile</MenubarTrigger>
                             <MenubarContent align="end">
-                                <MenubarLabel>Account</MenubarLabel>
-                                <MenubarItem>Profile</MenubarItem>
-                                <MenubarItem>Settings</MenubarItem>
-                                <MenubarItem>Logout</MenubarItem>
+                                <MenubarLabel><a href="./projects">Projects</a></MenubarLabel>
+                                <MenubarItem><a href="./portfolio">Portfolio</a></MenubarItem>
+                                <MenubarItem><a href="./settings">Settings</a></MenubarItem>
+                                <MenubarItem><a href="./">Logout</a></MenubarItem>
                             </MenubarContent>
                         </MenubarMenu>
                     </Menubar>
